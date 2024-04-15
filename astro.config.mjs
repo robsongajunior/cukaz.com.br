212e4ebe9c5d671a9e4dbf9d77ad1194ea8d866e
Robson Junior <robson.ga.junior@gmail.com>
2024-04-15 02:25:23 -0300

    chore: remove video and heavr img

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,14 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+// import mdx from '@astrojs/mdx';
 import vue from '@astrojs/vue';
-import react from '@astrojs/react';
-import sitemap from '@astrojs/sitemap';
+// import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://www.robsonjunior.dev/',
+  site: 'https://www.cukaz.com.br',
   // trailingSlash: 'always', // for server
   integrations: [
-        react(),
         vue(),
-        mdx(),
-        sitemap()
+        // mdx(),
+        // sitemap()
     ]
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,16 @@
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
-    schema: z.object({
-        title: z.string(),
-        meta_robots: z.string().default('index, follow'),
-        meta_description: z.string(),
-        categories: z.any().optional(),
-        date: z.string().or(z.date()).transform((val) => new Date(val)),
-        dateUpdate: z.string().optional().transform((str) => (str ? new Date(str) : undefined)),
-        heroImage: z.string().optional(),
-        heroAltImage: z.string().optional()
-    })
-});
+// const blog = defineCollection({
+//     schema: z.object({
+//         title: z.string(),
+//         meta_robots: z.string().default('index, follow'),
+//         meta_description: z.string(),
+//         categories: z.any().optional(),
+//         date: z.string().or(z.date()).transform((val) => new Date(val)),
+//         dateUpdate: z.string().optional().transform((str) => (str ? new Date(str) : undefined)),
+//         heroImage: z.string().optional(),
+//         heroAltImage: z.string().optional()
+//     })
+// });
 
-export const collections = { blog };
+export const collections = {  };
